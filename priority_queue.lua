9155689b3e7d663413b4427fb8afdc45a13b30a8
ozkriff <ozkriff@gmail.com>
2012-09-04 17:04:48 +0400

    priority_queue.lua: Rewrite using closure approach

--- priority_queue.lua
+++ priority_queue.lua
@@ -8,62 +8,54 @@
 --   end
 --   while not q:is_empty() do print(q:pop()) end
 
-local PriorityQueue = {}
-PriorityQueue.__index = PriorityQueue
+return function()
+  local self = {}
 
-function PriorityQueue:push(k, v)
-  assert(v ~= nil, "cannot push nil")
-  local t = self.nodes
-  local self = self.heap
-  local n = #self + 1 -- node position in heap array (leaf)
-  local p = (n - n % 2) / 2 -- parent position in heap array
-  self[n] = k -- insert at a leaf
-  t[k] = v
-  while n > 1 and t[self[p]] < v do -- climb heap?
-    self[p], self[n] = self[n], self[p]
-    n = p
-    p = (n - n % 2) / 2
-  end
-end
+  local heap = {}
+  local nodes = {}
 
-function PriorityQueue:pop()
-  local t = self.nodes
-  local self = self.heap
-  local s = #self
-  assert(s > 0, "cannot pop from empty heap")
-  local e = self[1] -- min (heap root)
-  local r = t[e]
-  local v = t[self[s]]
-  self[1] = self[s] -- move leaf to root
-  self[s] = nil -- remove leaf
-  t[e] = nil
-  s = s - 1
-  local n = 1 -- node position in heap array
-  local p = 2 * n -- left sibling position
-  if s > p and t[self[p]] < t[self[p + 1]] then
-    p = 2 * n + 1 -- right sibling position
-  end
-  while s >= p and t[self[p]] > v do -- descend heap?
-    self[p], self[n] = self[n], self[p]
-    n = p
-    p = 2 * n
-    if s > p and t[self[p]] < t[self[p + 1]] then
-      p = 2 * n + 1
+  self.push = function(k, v)
+    assert(v ~= nil, "cannot push nil")
+    local n = #heap + 1 -- node position in heap array (leaf)
+    local p = (n - n % 2) / 2 -- parent position in heap array
+    heap[n] = k -- insert at a leaf
+    nodes[k] = v
+    while n > 1 and nodes[heap[p]] < v do -- climb heap?
+      heap[p], heap[n] = heap[n], heap[p]
+      n = p
+      p = (n - n % 2) / 2
     end
   end
-  return e, r
-end
 
-function PriorityQueue:is_empty()
-  return self.heap[1] == nil
-end
+  self.pop = function()
+    local s = #heap
+    assert(s > 0, "cannot pop from empty heap")
+    local e = heap[1] -- min (heap root)
+    local r = nodes[e]
+    local v = nodes[heap[s]]
+    heap[1] = heap[s] -- move leaf to root
+    heap[s] = nil -- remove leaf
+    nodes[e] = nil
+    s = s - 1
+    local n = 1 -- node position in heap array
+    local p = 2 * n -- left sibling position
+    if s > p and nodes[heap[p]] < nodes[heap[p + 1]] then
+      p = 2 * n + 1 -- right sibling position
+    end
+    while s >= p and nodes[heap[p]] > v do -- descend heap?
+      heap[p], heap[n] = heap[n], heap[p]
+      n = p
+      p = 2 * n
+      if s > p and nodes[heap[p]] < nodes[heap[p + 1]] then
+        p = 2 * n + 1
+      end
+    end
+    return e, r
+  end
 
-function PriorityQueue.new()
-  local new_queue = {
-    heap = {},
-    nodes = {}
-  }
-  return setmetatable(new_queue, PriorityQueue)
-end
+  self.is_empty = function()
+    return heap[1] == nil
+  end
 
-return PriorityQueue
+  return self
+end
