3574f0e34d00b3bbdb642db610b87413185553d5
ozkriff <ozkriff@gmail.com>
2012-09-12 13:49:09 +0400

    marauder.lua: main(): Remove empty lines

--- marauder.lua
+++ marauder.lua
@@ -20,19 +20,15 @@ local TimeSystem = require 'time_system'
 function main()
   math.randomseed(os.time())
   local screen = Screen.new()
-
   local map = Map.new()
   map:set_size({y = 15, x = 15}) -- in tiles
   -- map.set_pos({y = 1, x = 1}) -- TODO: in pixels
   map:set_screen(screen) -- TODO: map_viewer
-
   local pathfinder = Pathfinder.new()
   pathfinder:set_map(map)
-
   local log = Log.new()
   log:set_pos({y = 300, x = 10}) -- in pixels
   log:set_screen(screen) -- TODO: log viewer?
-
   local time_system = TimeSystem.new()
   local game = Game.new()
   game:set_screen(screen)
