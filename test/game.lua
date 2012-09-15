fad4328355d83f529f53a9d19104961922dd57a6
ozkriff <ozkriff@gmail.com>
2012-09-15 14:17:18 +0400

    test/game.lua: Fix formating

--- test/game.lua
+++ test/game.lua
@@ -4,8 +4,10 @@ local Symbols = require 'symbols'
 
 local function test_unittype_to_char()
   local game = Game.new()
-  Assert.is_equal(game.unit_type_to_char('player'), Symbols.AT)
-  Assert.is_equal(game.unit_type_to_char('enemy'), Symbols.Z)
+  Assert.is_equal(game.unit_type_to_char('player'),
+      Symbols.AT)
+  Assert.is_equal(game.unit_type_to_char('enemy'),
+      Symbols.Z)
   Assert.is_nil(game.unit_type_to_char('ololo'))
 end
 
